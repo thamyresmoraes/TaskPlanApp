224ac87bbec81bd8d2bf0212e5ba7b41557ddf62
thamyresmoraes <thamyresmoraes@live.com>
2023-11-19 18:41:54 -0300

    update

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+require("firebase-functions/v2/https");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
